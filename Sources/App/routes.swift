0b884a12a8c76b1d0b0c50de0359dca551f55cf8
Ben Nortier <ben@bjnortier.com>
2019-08-21 21:44:55 +0200

    Add index html & bundle

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,10 @@
 import Vapor
 import SQLite
 
+fileprivate struct AirportSearchRequest: Content {
+    var input: String
+}
+
 fileprivate struct CO2eRequest: Content {
     var from: Int
     var to: Int
@@ -11,13 +15,36 @@ fileprivate struct CO2eResponse: Content {
     let kgCO2e: Double
 }
 
-/// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    router.get("airports", String.parameter) { req -> [Airport] in
-        let searcher = try req.make(AirportSearcher.self)
-        let searchString = try req.parameters.next(String.self)
-        let airports = searcher.query(searchString)
-        return airports
+    router.get { req -> HTTPResponse in
+        let res = HTTPResponse(
+            status: .ok,
+            headers: ["Content-Type": "text/html"],
+            body: """
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CO2e</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+  </head>
+  <body>
+    <div id="root"></div>
+    <script src="/index.bundle.js"> type="text/javascript"></script>
+  </body>
+</html>
+""")
+        return res
+    }
+
+    router.post("airportsearch") { req -> Future<[Airport]> in
+        return try req.content.decode(AirportSearchRequest.self).map(to: [Airport].self) { airportSearchRequest in
+            let searcher = try req.make(AirportSearcher.self)
+            let airports = searcher.query(airportSearchRequest.input)
+            return airports
+        }
     }
     
     router.post("co2e") { req -> Future<CO2eResponse> in
